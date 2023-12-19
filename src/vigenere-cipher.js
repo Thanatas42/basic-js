7efacd3ce0bb2617599e13321cc5a6c3d366e5d7
Thanatas42 <dmitrodonskoy@yandex.ru>
2023-12-19 14:01:00 +0300

    feat: 74 pass completed

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  _checkArgs(message, key) {
+    if (typeof message === 'undefined' || typeof key === 'undefined')
+      throw new Error('Incorrect arguments!');
+  }
+
+  encrypt(message, key) {
+    this._checkArgs(message, key);
+    message = message.toUpperCase();
+
+    let arrMessage = message.split('');
+    const stringMessage = arrMessage.join('');
+    while (key.length < stringMessage.length) {
+      key += key;
+    }
+    key = key.slice(0, stringMessage.length).toUpperCase().split('');
+
+    let arr = arrMessage.map((char, index) => {
+      if (char === ' ') {
+        key.splice(index, 0, ' ');
+      }
+      const charNum = char.charCodeAt();
+
+      if (65 <= charNum && charNum <= 90) {
+        let criptChar = charNum + (key[index].charCodeAt() - 65);
+
+        if (criptChar > 90) {
+          criptChar = criptChar - 26;
+        }
+
+        return String.fromCharCode(criptChar);
+      } else {
+        return char;
+      }
+    });
+
+    return this.type ? arr.join('') : arr.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    this._checkArgs(message, key);
+
+    let arrMessage = message.split('');
+    const stringMessage = message.split(' ').join('');
+    while (key.length < stringMessage.length) {
+      key += key;
+    }
+    key = key.slice(0, stringMessage.length).toUpperCase().split('');
+
+    let arr = arrMessage.map((char, index) => {
+      if (char === ' ') {
+        key.splice(index, 0, ' ');
+      }
+      const charNum = char.charCodeAt();
+      if (65 <= charNum && charNum <= 90) {
+
+        let decriptChar = charNum - (key[index].charCodeAt() - 65);
+
+        if (decriptChar < 65) {
+          decriptChar = decriptChar + 26;
+        }
+        return String.fromCharCode(decriptChar);
+      } else {
+        return char;
+      }
+    });
+
+    return this.type ? arr.join('') : arr.reverse().join('');
   }
 }
 
